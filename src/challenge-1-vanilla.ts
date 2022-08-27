7789d5fa22b49627dfec42c65d070dabb1356449
veyronB2 <lokyt2@gmail.com>
2022-08-27 21:37:49 +0100

    introduced clousers

--- src/challenge-1-vanilla.ts
+++ src/challenge-1-vanilla.ts
@@ -4,7 +4,7 @@
 import { initialState } from "./constants";
 import { getFilteredCharacters, fetchAllData, calculatePower } from "./utils";
 
-let state = initialState;
+//let state = initialState;
 
 const selectors = {
   getTableBody() {
@@ -21,118 +21,142 @@ const selectors = {
   },
 };
 
-function renderRows() {
-  const tbody = selectors.getTableBody(); //get table body
-  // remove any present rows before rendering
-  if (tbody.firstChild) {
-    removeRows();
-  }
-
-  state.filteredCharacters.forEach((character) => {
-    const row = document.createElement("tr");
-    const name = document.createElement("td");
-    const height = document.createElement("td");
-    const mass = document.createElement("td");
-    const power = document.createElement("td");
-
-    name.innerText = character.name;
-    height.innerText = character.height;
-    mass.innerText = character.mass;
-    power.textContent = calculatePower(character, state.multiplier);
-
-    row.append(name, height, mass, power);
-    tbody.appendChild(row);
-  });
-}
-
-function initializeState(characters) {
-  state = {
-    ...state,
-    filteredCharacters: getFilteredCharacters({
-      characters,
-      query: state.query,
-    }),
-  };
-}
-
-function removeRows() {
-  const tbody = selectors.getTableBody();
-  //remove all children if first child not empty
-  while (tbody.firstChild) {
-    tbody.firstChild.remove();
-  }
-}
-
-function updateState({ characters, query, multiplier }) {
-  if (typeof query === "string") {
+function stateControl() {
+  let state = initialState;
+  function initializeState(characters) {
     state = {
       ...state,
-      query,
+      filteredCharacters: getFilteredCharacters({
+        characters,
+        query: state.query,
+      }),
+    };
+  }
+
+  function updateState({ characters, query, multiplier }) {
+    if (typeof query === "string") {
+      state = {
+        ...state,
+        query,
+        filteredCharacters: getFilteredCharacters({
+          characters,
+          query,
+        }),
+      };
+    }
+
+    if (multiplier) {
+      state = {
+        ...state,
+        multiplier,
+      };
+    }
+  }
+
+  function resetState(characters) {
+    const { query } = initialState;
+
+    state = {
+      ...initialState,
       filteredCharacters: getFilteredCharacters({
         characters,
         query,
       }),
     };
+    Renderer().updateInputValues(state);
+    Renderer().removeRows();
+    Renderer().renderRows(state);
   }
 
-  if (multiplier) {
-    state = {
-      ...state,
-      multiplier,
-    };
+  function getState() {
+    return state;
   }
-}
-
-function updateTableRows() {
-  removeRows();
-  renderRows();
-}
-
-function resetState(characters) {
-  const { query } = initialState;
-
-  state = {
-    ...initialState,
-    filteredCharacters: getFilteredCharacters({
-      characters,
-      query,
-    }),
+  return {
+    initializeState,
+    updateState,
+    resetState,
+    getState,
   };
-  updateInputValues();
-  updateTableRows();
 }
 
-function updateInputValues() {
-  selectors.getFilterInput().value = state.query;
-  selectors.getMultiplierInput().value = state.multiplier;
+function Renderer() {
+  function renderRows(state) {
+    const tbody = selectors.getTableBody(); //get table body
+    // remove any present rows before rendering
+
+    if (tbody.firstChild) {
+      removeRows();
+    }
+
+    state.filteredCharacters.forEach((character) => {
+      const row = document.createElement("tr");
+      const name = document.createElement("td");
+      const height = document.createElement("td");
+      const mass = document.createElement("td");
+      const power = document.createElement("td");
+
+      name.innerText = character.name;
+      height.innerText = character.height;
+      mass.innerText = character.mass;
+      power.textContent = calculatePower(character, state.multiplier);
+
+      row.append(name, height, mass, power);
+      tbody.appendChild(row);
+    });
+  }
+
+  function removeRows() {
+    const tbody = selectors.getTableBody();
+    //remove all children if first child not empty
+    while (tbody.firstChild) {
+      tbody.firstChild.remove();
+    }
+  }
+
+  function updateInputValues(state) {
+    selectors.getFilterInput().value = state.query;
+    selectors.getMultiplierInput().value = state.multiplier;
+  }
+  return {
+    renderRows,
+    removeRows,
+    // updateTableRows,
+    updateInputValues,
+  };
 }
 
 export async function runVanillaApp() {
   /*************** fetch data ***************************/
   const characters = await fetchAllData();
+  const setState = stateControl();
+  const renderer = Renderer();
 
   //update state with fetched info
-  initializeState(characters);
+  setState.initializeState(characters);
 
   //render table rows
-  renderRows();
+  renderer.renderRows(setState.getState());
 
   // /***************************  Event listeners **************************************************************************/
   selectors.getMultiplierInput().addEventListener("change", (e) => {
-    updateState({ characters, multiplier: Number(e.target.value) });
-    updateTableRows();
+    setState.updateState({
+      characters,
+      multiplier: Number(e.target.value),
+    });
+    renderer.removeRows();
+    renderer.renderRows(setState.getState());
   });
 
   selectors.getFilterInput().addEventListener("input", (e) => {
-    updateState({ characters, query: e.target.value });
-    updateTableRows();
+    setState.updateState({ characters, query: e.target.value });
+    renderer.removeRows();
+    renderer.renderRows(setState.getState());
   });
-
-  selectors.getContainer().addEventListener("keydown", escapeKeyHandler);
 
   function escapeKeyHandler(e) {
     if (e.code === "Escape") {
-      resetState(characters);
+      setState.resetState(characters);
     }
   }
+  selectors.getContainer().addEventListener("keydown", escapeKeyHandler);
 }
